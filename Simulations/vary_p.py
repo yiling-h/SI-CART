79194b6c185f93dce457e15aaf96f1bac1ff577e
yiling-h <yilingh@umich.edu>
2024-11-22 21:29:18 -0500

    vary p update

--- Simulations/vary_p.py
+++ Simulations/vary_p.py
@@ -155,7 +155,7 @@ def randomized_inference(reg_tree, sd_y, y, mu, level=0.1):
         pval, dist, contrast, norm_contrast, obs_tar, logW, suff, sel_probs \
             = (reg_tree.condl_node_inference(node=node,
                                              ngrid=10000,
-                                             ncoarse=100,
+                                             ncoarse=300,
                                              grid_w_const=5,
                                              reduced_dim=1,
                                              sd=sd_y,
