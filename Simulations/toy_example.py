213c08ced191a05a708ca92e9a7065563ba65c36
yiling-h <yilingh@umich.edu>
2025-02-28 01:10:54 -0500

    Denser grid

--- Simulations/toy_example.py
+++ Simulations/toy_example.py
@@ -127,7 +127,7 @@ def randomized_inference(reg_tree, sd_y, y, mu, level=0.1):
         pval, dist, contrast, norm_contrast, obs_tar, logW, suff, sel_probs \
             = (reg_tree.condl_node_inference(node=node,
                                              ngrid=10000,
-                                             ncoarse=50,
+                                             ncoarse=200,
                                              grid_w_const=5,
                                              reduced_dim=None,
                                              sd=sd_y,
