aec18eb3cdd9041dde0455e90b91b5d9cdcbb524
yiling-h <yilingh@umich.edu>
2025-03-17 11:09:31 -0400

    r=5

--- Simulations/toy_example.py
+++ Simulations/toy_example.py
@@ -132,7 +132,7 @@ def randomized_inference(reg_tree, sd_y, y, mu, level=0.1):
                                              grid_w_const=20,
                                              query_size=200,
                                              query_grid=True,
-                                             reduced_dim=10,
+                                             reduced_dim=5,
                                              sd=sd_y,
                                              use_cvxpy=False))
         """pval, dist, contrast, norm_contrast, obs_tar, logW, suff, sel_probs, _ \
