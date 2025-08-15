646816779e677ce1017f6fef7204adc70cc9c87e
yiling-h <yilingh@umich.edu>
2025-08-14 20:36:07 -0400

    Sparser grid

--- Simulations/toy_example.py
+++ Simulations/toy_example.py
@@ -130,7 +130,7 @@ def randomized_inference(reg_tree, sd_y, y, mu, prop, noise_sd=1,
          sel_probs, ref_hat_layer, marginal) \
             = (reg_tree.condl_node_inference(node=node,
                                              ngrid=10000,
-                                             ncoarse=500,
+                                             ncoarse=200,
                                              grid_w_const=20*noise_sd,
                                              query_size=100,
                                              query_grid=True,
